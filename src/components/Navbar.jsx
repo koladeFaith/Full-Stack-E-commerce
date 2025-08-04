cab6c1693e6653b5b8304ef8eed769a1c7a2a71e
Kolade Faith <koladefaith1000@gmail.com>
2025-08-04 13:01:56 +0100

    Work done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <nav className=" bg-purple-700 flex items-center justify-between gap-2 px-2 px-md-4 py-3">
+      <nav className=" bg-purple-700 flex items-center justify-between gap-2 px-4 px-md-5  py-3">
         {/* Logo */}{" "}
         <div className=" flex items-center justify-center gap-3">
           <div className="md:hidden text-white text-[25px] mb-1">
@@ -13,7 +13,7 @@ const Navbar = () => {
           </div>
           <div className=" animate-fade-in">
             <div className="flex items-center justify-center space-x-4 ">
-              <h1 className="text-xl md:text-2xl  font-bold text-white drop-shadow-2xl">
+              <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-white drop-shadow-2xl">
                 Luxora
               </h1>
             </div>
@@ -26,18 +26,18 @@ const Navbar = () => {
           <Link className="no-underline text-white">About</Link>
           <Link className="no-underline text-white">Contact</Link>
         </div>
-        <div className="flex items-center justify-center gap-3 text-white text-[20px]">
-          <div className="sm:hidden md:flex relative w-full max-w-sm text-[18px] ">
+        <div className="flex items-center justify-center gap-3 text-white ">
+          <div className="relative w-full hidden lg:block text-[16px] ">
             <input
               type="text"
               placeholder="Search for books..."
-              className="w-full pl-10 pr-4 py-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className=" w-full pl-10 pr-4 text-black py-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+            <FaSearch className=" absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
           </div>
-          <FaSearch className="md:hidden" />
-          <FaShoppingCart />
-          <FaUser />
+          <FaSearch className="lg:hidden text-[25px]" />
+          <FaShoppingCart className=" text-[25px]" />
+          <FaUser className=" text-[25px]" />
         </div>
       </nav>
     </>
