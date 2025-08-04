6f8b99e4c2d04b66c33605d5ec632e4070d558d1
Kolade Faith <koladefaith1000@gmail.com>
2025-08-04 12:10:59 +0100

    Work done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,45 @@
 import React from "react";
-
+import { FaList, FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div>
-        {/* Logo */}
-        <div className="mb-12 animate-fade-in">
-          <div className="flex items-center justify-center space-x-4 mb-2">
-            <div className="w-16 h-16 bg-gradient-to-br from-purple-400 to-blue-500 rounded-2xl flex items-center justify-center shadow-2xl animate-pulse">
-              <svg
-                className="w-10 h-10 text-white"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
-              </svg>
-            </div>
-            <h1 className="text-6xl font-bold text-white drop-shadow-2xl">
-              Luxora
-            </h1>
+      <nav className=" bg-purple-700 flex items-center justify-between gap-2 px-2 px-md-4 py-3">
+        {/* Logo */}{" "}
+        <div className=" flex items-center justify-center gap-3">
+          <div className="md:hidden text-white text-[25px] mb-1">
+            {" "}
+            <FaList />
           </div>
-          <div className="w-24 h-1 bg-gradient-to-r from-purple-400 to-blue-500 mx-auto rounded-full shadow-lg animate-pulse"></div>
+          <div className=" animate-fade-in">
+            <div className="flex items-center justify-center space-x-4 ">
+              <h1 className="text-xl md:text-2xl  font-bold text-white drop-shadow-2xl">
+                Luxora
+              </h1>
+            </div>
+            <div className="w-15 md:w-24 h-1 bg-gradient-to-r from-purple-400 to-blue-500 mx-auto rounded-full shadow-lg animate-pulse"></div>
+          </div>
         </div>
-        <div>
-          {" "}
-          <li>Home</li>
-          <li>Products</li>
-          <li>Categories</li>
-          <li>Order</li>
-          <li>Contact us</li>
+        <div className="list-none hidden md:flex items-center justify-center gap-4 text-white fw-bold text-[18px]">
+          <Link className="no-underline text-white">Home</Link>
+          <Link className="no-underline text-white">Categories</Link>
+          <Link className="no-underline text-white">About</Link>
+          <Link className="no-underline text-white">Contact</Link>
         </div>
-        <div>Cart</div>
-      </div>
+        <div className="flex items-center justify-center gap-3 text-white text-[20px]">
+          <div className="sm:hidden md:flex relative w-full max-w-sm text-[18px] ">
+            <input
+              type="text"
+              placeholder="Search for books..."
+              className="w-full pl-10 pr-4 py-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+          </div>
+          <FaSearch className="md:hidden" />
+          <FaShoppingCart />
+          <FaUser />
+        </div>
+      </nav>
     </>
   );
 };
